c246f473ae657232d8268674c5b9f8decb55b04a
Norayr <asoyannorayr99@gmail.com>
2023-11-02 21:07:31 +0300

    add commit

--- src/AgeOfPeople.java
+++ src/AgeOfPeople.java
@@ -1,11 +1,9 @@
 public class AgeOfPeople {
     public static void main(String[] args) {
-       byte age1=50;
-       byte age2=13;
-        getAgeDiff(age1,age2);
+        byte calculategetAgeDiff = getAgeDiff((byte) 10, (byte) 25);
+        System.out.println(calculategetAgeDiff);
     }
-    public static void getAgeDiff(byte age1, byte age2) {
-
-        System.out.println((byte) Math.abs(age1 - age2));
+    public static byte getAgeDiff(byte age1, byte age2) {
+        return (byte) Math.abs(age1 - age2);
     }
 }
